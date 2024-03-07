67a35d00cd2763346e179776080404f1ea5747a8
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-03-07 12:51:07 +0530

    Update code 5.C program to check whether a given string belongs to the language defined by a Context Free Grammar (CFG) .c

--- Day 3 Toc Codes/code 5.C program to check whether a given string belongs to the language defined by a Context Free Grammar (CFG) .c
+++ Day 3 Toc Codes/code 5.C program to check whether a given string belongs to the language defined by a Context Free Grammar (CFG) .c
@@ -1,2 +1,38 @@
-5.	Write a C program to check whether a given string belongs to the language defined by a Context Free Grammar (CFG)
-S → 0S0 | A	A → 1A | ε
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool belongsToCFG(char *str) {
+    int len = strlen(str);
+    int i = 0;
+
+    while (i < len && str[i] == '0')
+        i++;
+
+    while (i < len && str[i] == '0')
+        i++;
+
+    if (i == len)
+        return true;
+
+    while (i < len && str[i] == '1')
+        i++;
+
+    if (i == len)
+        return true;
+
+    return false;
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    if (belongsToCFG(str))
+        printf("The string belongs to the language defined by the CFG.\n");
+    else
+        printf("The string does not belong to the language defined by the CFG.\n");
+
+    return 0;
+}
